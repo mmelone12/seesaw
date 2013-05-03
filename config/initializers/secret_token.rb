ec06c68abd326c90b9166be9b1dc5895f1736e9e
mmelone12 <matt.m.melone@gmail.com>
2013-05-02 17:04:51 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seesaw::Application.config.secret_token = 'fabde2199a16473276984d37e5196e0ec59776cd6a2bdcce40de57ad12c2f88f45dd973817dc30da33ffd19103009a6554ef7c52ba665fa696bc2db5f446f46c'
